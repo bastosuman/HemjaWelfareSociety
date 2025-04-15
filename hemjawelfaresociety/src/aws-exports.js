69116e6236428d0761308a229c9cad0ee07d4fba
bastosuman <bastolasuman@gmail.com>
2025-04-15 08:33:21 -0400

    Configure AWS Amplify

---
+++ hemjawelfaresociety/src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",  // or your preferred region
+    "aws_cognito_identity_pool_id": "YOUR_IDENTITY_POOL_ID",
+    "aws_cognito_region": "us-east-1",  // or your preferred region
+    "aws_user_pools_id": "YOUR_USER_POOL_ID",
+    "aws_user_pools_web_client_id": "YOUR_WEB_CLIENT_ID"
+};
+
+export default awsmobile; 
